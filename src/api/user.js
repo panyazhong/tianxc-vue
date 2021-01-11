358a2d4fb0962e89a61a1e03e8e5d2ef995ceb76
panyazhong <pyz1226149198@gmail.com>
2021-01-11 23:37:54 +0800

    用户行为监测统计初步设计

--- src/api/user.js
+++ src/api/user.js
@@ -1,11 +1,16 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
 
+/**
+ * 记录用户登录时间， 登出时间在用户注销时记录
+ * ？？用户ip地址
+ * ？？用户增长
+ */
 export function login(data) {
   return request({
     url: 'api/user/login',
     method: 'post',
     data: data,
-  })
+  });
 }
 
 export function register(data) {
@@ -13,7 +18,7 @@ export function register(data) {
     url: '/api/user/register',
     method: 'post',
     data: data,
-  })
+  });
 }
 
 export function delUser(params) {
@@ -21,14 +26,14 @@ export function delUser(params) {
     url: '/api/user/editUser',
     method: 'delete',
     params: params,
-  })
+  });
 }
 
 export function getUserInfo() {
   return request({
     url: 'api/user/getUserInfo',
     method: 'get',
-  })
+  });
 }
 
 export function getUsers(params) {
@@ -36,14 +41,14 @@ export function getUsers(params) {
     url: 'api/user/getUsers',
     method: 'get',
     params: params,
-  })
+  });
 }
 
 export function getRoles() {
   return request({
     url: 'api/role/getRoles',
     method: 'get',
-  })
+  });
 }
 
 export function changePwd(data) {
@@ -51,5 +56,14 @@ export function changePwd(data) {
     url: '/api/user/changePwd',
     method: 'post',
     data: data,
-  })
+  });
+}
+
+// 用户注销  统计用户在线时长等信息
+export function logOut(params) {
+  return request({
+    url: '/api/user/logout',
+    method: 'post',
+    data: params,
+  });
 }
