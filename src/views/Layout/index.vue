caaec7ea74b144863b8582023011066b3d1cef5b
panyazhong <pyz1226149198@gmail.com>
2021-01-12 17:33:57 +0800

    用户行为记录

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -12,6 +12,10 @@
 import HeaderCom from './components/header'
 import MenuCom from './components/menu'
 import { mapGetters } from 'vuex'
+import { getToken, getUserBehavior, removeUserBehavior } from '@/utils/cookie'
+
+import { sendBeacon } from '@/api/beacon'
+
 export default {
   data() {
     return {
@@ -24,32 +28,61 @@ export default {
     MenuCom
   },
   mounted() {
-    this.loadTime = new Date().getTime()
-    console.log(navigator.sendBeacon)
+    this.loadTime = Date.now()
     window.onbeforeunload = () => {
-      this.leaveTime = new Date().getTime()
+      this.leaveTime = Date.now()
 
-      // 当时间大于3s时才计算用户行为信息
-      if (this.leaveTime - this.loadTime > 3 * 1000) {
-        if (navigator.sendBeacon) {
-          navigator.sendBeacon()
-        } else {
-          this.sendBehavior()
+      // 当时间大于6s时才计算用户行为信息
+      if (!navigator.sendBeacon && token) {
+        if (this.leaveTime - this.loadTime > 6 * 1000) {
+          let fd = this.gengerateFormData(token)
+
+          let xhr = new XMLHttpRequest()
+          let url = ENV === 'development' ?
+            `http://localhost:3000/api/behavior/send` :
+            `/imon/api/v1.0/user/use_information`
+          xhr.open('post', url, false)
+          xhr.send(fd)
         }
+        return false
       }
-
-      return false
     }
+
+    window.addEventListener("unload", (e) => {
+      this.sendBeacon()
+    })
   },
   computed: {
     ...mapGetters(['device'])
   },
   methods: {
+    gengerateFormData(token) {
+      let behavior = getUserBehavior()
+      console.log(behavior)
+      let fd = new FormData()
+      fd.append('token', token)
+      fd.append('load_time', this.loadTime)
+      fd.append('leave_time', this.leaveTime)
+      fd.append('behavior', behavior)
+      fd.append('type', 'leave')
+
+      return fd
+    },
     // 上传用户信息
     sendBeacon() {
+      let token = getToken()
+      if (navigator.sendBeacon && !!token) {
+        this.leaveTime = Date.now()
+        if (this.leaveTime - this.loadTime > 6 * 1000) {
+          let fd = this.gengerateFormData(token)
 
+          let url = `http://localhost:3000/api/behavior/send`
+          navigator.sendBeacon(url, fd)
+          removeUserBehavior()
+        }
+      }
     },
-    sendBehavior() {}
+    unloadHandler() {}
   }
 }
 </script>
